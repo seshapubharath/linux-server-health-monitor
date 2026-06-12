dc609dbd377ea42380e8df01b53c64e2c5d23f10
Bharath Chand Seshapu <147697996+seshapubharath@users.noreply.github.com>
2026-06-12 23:55:24 +0530

    Create monitor.sh

---
+++ scripts/monitor.sh
@@ -0,0 +1,29 @@
+========================
+Server Health Report
+========================
+
+Date:
+2025-06-12
+
+Hostname:
+localhost
+
+Uptime:
+3 hours
+
+CPU Usage:
+15%
+
+Memory Usage:
+40%
+
+Disk Usage:
+52%
+
+Logged In Users:
+bharath
+
+Top Processes:
+firefox
+chrome
+python
